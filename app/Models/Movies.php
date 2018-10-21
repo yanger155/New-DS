f41f03f5e34e57f4826dfb920644333d7202d08b
yanger <info@yangxueying.com>
2018-10-21 20:21:09 +0800

    laravel admin 重新安装成功并创建了测试用的MoviesController控制器进行模拟操作

---
+++ app/Models/Movies.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movies extends Model
+{
+    protected $table = "brand";//要连接的表名称
+    public $timestamps = false;//将时间戳设置为false，否则数据表没有对应字段（create_at等字段）就会报错
+    
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //运行install时，可能会报mysql1071错误
+        Schema::defaultStringLength(191);
     }
 
     /**
