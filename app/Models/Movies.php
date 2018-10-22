fe62c4f57efe749f81a353a40736b075f01a3260
yanger <info@yangxueying.com>
2018-10-22 10:30:31 +0800

    前端页面相关样式成功迁移至public目录下。lavarel admin 暂待定，开始页面的迁移连接

--- app/Models/Movies.php
+++ app/Models/Movies.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movies extends Model
 {
-    protected $table = "brand";//要连接的表名称
+    protected $table = "movies";//要连接的表名称
     public $timestamps = false;//将时间戳设置为false，否则数据表没有对应字段（create_at等字段）就会报错
     
 }
